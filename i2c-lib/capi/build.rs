15d0fa7faccbbd7aa964d7f07f8e48f89b79f2ae
Fulup Ar Foll <fulup@iot.bzh>
2023-12-12 21:38:35 +0100

    force rlib search path

--- i2c-lib/capi/build.rs
+++ i2c-lib/capi/build.rs
@@ -6,11 +6,17 @@
  * License: $RP_BEGIN_LICENSE$ SPDX:MIT https://opensource.org/licenses/MIT $RP_END_LICENSE$
  *
 */
-extern crate bindgen;
+use std::env;
 
 fn main() {
     // invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=capi/capi-map.h");
+    println!("cargo:rustc-link-search=/usr/local/lib64");
+    if let Ok(value) = env::var("CARGO_TARGET_DIR") {
+        if let Ok(profile) = env::var("PROFILE") {
+            println!("cargo:rustc-link-search=crate={}{}", value, profile);
+        }
+    }
 
     let header = "
     // -----------------------------------------------------------------------
@@ -22,26 +28,21 @@ fn main() {
     //     - src/capi/capi-map.h for C prototype inputs
     // -----------------------------------------------------------------------
     ";
-    let _capi_map
-     = bindgen::Builder::default()
+    let _capi_map = bindgen::Builder::default()
         .header("capi/capi-map.h") // Pionix C++ prototype wrapper input
         .raw_line(header)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .derive_debug(false)
         .layout_tests(false)
-
         .allowlist_function("open")
         .allowlist_function("close")
         .allowlist_function("ioctl")
         .allowlist_function("i2c_smbus_read_.*")
         .allowlist_function("i2c_smbus_write_.*")
-
         .allowlist_var("BUS_I2C_.*")
-
         .allowlist_function("__errno_location")
         .allowlist_function("errno")
         .allowlist_function("strerror_r")
-
         .generate()
         .expect("Unable to generate _capi-map.rs");
 
